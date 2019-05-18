339aeb76637672c5d95140f250d0545337fc4566
Plasmatic <moses@mosesxu.net>
2019-05-17 21:06:39 -0400

    added new output mode for use with betterwebvote JSON import

--- data_collection/collector.js
+++ data_collection/collector.js
@@ -3,6 +3,7 @@ const CollectorUtils = require('./collector_utils.js');
 const Deque = require('./deque.js');
 
 const fs = require('fs');
+const util = require('util');
 
 const media_urls = require('./urls/media_urls.json');
 const config = require('./config.json');
@@ -13,17 +14,17 @@ const HREFS_REGEX = /href="(.*?)"/g;
 * Configuration Options:
 * 
 * mode: What data is collected.  Currently the modes are:
-*   - title_only: Only the title of each article is collected
+*   - title_with_bias: Only the title of each article is collected and is attached with its source and the bias of its source
+*   - title_only: The headlines are collected and outputted as a list
 * article_count: For each news source, how many articles will be selected
 * log_file: Path to the log file
-* source_output_file: Output file with the results of the crawl grouped by which source they come from
-* training_output_file: Output file with the results of the crawl as a list, with each entry containing the data and bias value of the source
+* output_file: Output file path
 * debug: Debug mode.  Among other things it restricts the amount of sources crawled to make testing faster
 */
 
+const MODES = new Set(['title_with_bias', 'title_only']);
+
 var logFile = fs.createWriteStream(config.log_file);
-var sourceOutput = {};
-var trainingOutput = []
 
 /**
  * Logger function
@@ -57,7 +58,7 @@ async function crawl(startUrl, urlMatch){
         return new Promise(function(resolve, _){
             Mercury.parse(url)
                 .then(parseData => {
-                    if(config.mode == 'title_only'){
+                    if(config.mode === 'title_with_bias' || config.mode === 'title_only'){
                         vis[url] = {
                             "title": parseData.title
                         };
@@ -105,30 +106,47 @@ async function crawl(startUrl, urlMatch){
     return vis;
 }
 
-if(config.debug)
-    log('Debug mode is ON');
-
-// Horribly un-js like isn't it?
-while(logFile.pending){}
-log(`Log file ready! Beginning collection...`);
-
+// Actual Data Collection
 (async function(){
+    // [ ====================================================================================================================================== ]
+    //                                                               Initialization
+    // [ ====================================================================================================================================== ]
+
+    if (!MODES.has(config.mode)) {
+        log(`Invalid mode ${config.mode}!`);
+        return;
+    }
+    
+    if(config.debug)
+        log('Debug mode is ON');
+    
+    // Horribly un-js like isn't it?
+    while(logFile.pending){}
+    log(`Log file ready! Beginning collection...`);
+    
+    // [ ====================================================================================================================================== ]
+    //                                                           Begin data collection loop
+    // [ ====================================================================================================================================== ]
+
+    var output = [];
     var ctr = 0; // Counter for debugging purposes
     var urls = media_urls.media_urls;
     for(var url in urls){
         log(`[ ==== ] Attempting to crawl ${url}... [ ==== ]`);
         
-        var currOutput = await crawl(urls[url][0].homepage, url), trainingData = Array.from(Object.values(currOutput));
-        sourceOutput[url] = currOutput;
-        for(var i = 0; i < trainingData.length; i++)
-            trainingOutput.push({data: trainingData[i], bias: urls[url][0].bias});
+        var currOutput = await crawl(urls[url][0].homepage, url), currData = Array.from(Object.values(currOutput));
+        for(var i = 0; i < currData.length; i++) {
+            if (config.mode === 'title_with_bias')
+                output.push({data: currData[i].title, bias: urls[url][0].bias});
+            else if (config.mode === 'title_only')
+                output.push(currData[i].title);
+        }
 
         if(config.debug && ++ctr >= 7)
             break;
     }
 
-    fs.writeFileSync(config.source_output_file, JSON.stringify(sourceOutput));
-    fs.writeFileSync(config.training_output_file, JSON.stringify(trainingOutput));
+    fs.writeFileSync(config.output_file, JSON.stringify(output));
 
     log('Crawl complete!');
 })();
